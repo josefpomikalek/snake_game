24ab9b4218ddd3903ff79cba36b5103b7590c64e
josefpomikalek <josef_pomikalek@yahoo.com>
2024-09-28 23:34:19 +0200

    Improve snake moving from the back to the head.

--- main.py
+++ main.py
@@ -23,11 +23,15 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
         
-
-
+    for seg_num in range(len(segments) -1, 0, -1):    
+        # It is like range(start= len(segments) -1, stop= 0, step= -1)
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num -1].ycor()
+        segments[seg_num].goto(new_x, new_y)
+    segments[0].forward(20)
+    # The snake moves in such a way that gradually all segments from the back except 
+    # for the last [0], i.e. head, accept the coordinates of the segment in front of them.
 
 
 
