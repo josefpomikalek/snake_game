785c49c35dd7e1305cdbaa19f99476768ea751c6
josefpomikalek <josef_pomikalek@yahoo.com>
2024-09-28 23:06:43 +0200

    Improve the move of snake using tracer() and time module.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from turtle import Screen, Turtle
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-
+screen.tracer(0)
 
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
@@ -17,11 +18,14 @@ for position in starting_positions:
     new_segment.goto(position)
     segments.append(new_segment)
 
+
 game_is_on = True
 while game_is_on:
+    screen.update()
+    time.sleep(0.1)
     for seg in segments:
         seg.forward(20)
-
+        
 
 
 
