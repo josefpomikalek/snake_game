fc107e757a02c9e900b3e74d056a7f7a3178854b
josefpomikalek <josef_pomikalek@yahoo.com>
2024-09-29 14:02:59 +0200

    Create constants for directions and use them in code.

--- snake.py
+++ snake.py
@@ -2,6 +2,11 @@ from turtle import Turtle
 
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 
 class Snake:
 
@@ -29,13 +34,13 @@ class Snake:
         # for the last [0], i.e. head, accept the coordinates of the segment in front of them.
 
     def up(self):
-        self.head.setheading(90)
+        self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        self.head.setheading(LEFT)
 
     def right(self):
-        self.head.setheading(0)   
+        self.head.setheading(RIGHT)   
