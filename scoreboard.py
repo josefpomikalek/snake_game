cf3325ea483fdd00fae0ffafa29f7cff02cf89c3
josefpomikalek <josef_pomikalek@yahoo.com>
2024-09-30 21:12:47 +0200

    Add new atribute high_score to scoreboard.py.file.

--- scoreboard.py
+++ scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
